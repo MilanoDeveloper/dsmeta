0862b4dce48975c57269fc4507f5eec3c90c2597
Gabriel Milano <milano.developer@gmail.com>
2022-09-06 18:57:25 -0300

    First Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
   return(
+    <>
     <h1>Olá mundo!</h1>
+    <NotificationButton/>
+    <NotificationButton/>
+    </>
   )
 
 }
